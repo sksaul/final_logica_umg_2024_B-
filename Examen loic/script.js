8afa054a4199365d9c568cf1da9ea07b660da006
sksaul <161138202+sksaul@users.noreply.github.com>
2024-06-15 10:31:09 -0600

    final
    hola

---
+++ Examen loic/script.js
@@ -0,0 +1,14 @@
+let in1 = 0;
+let in22;
+let total;
+console.log(numero1);
+function hacer(){
+numero1 = parseFloat(document.getElementById('in1').value);
+numero2 = parseFloat(document.getElementById('in2').value);
+total = in1 + in2;
+document.getElementById('res').value = total;
+}
+
+
+
+
